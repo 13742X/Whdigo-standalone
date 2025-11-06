1b7e22c035e69c8e7a9d9810c86f503fcfd65aa2
meta.inverse <13742x@gmail.com>
2025-11-06 09:36:04 +0000

    v1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Whdigo-standalone
 Offline Ultra Secure Password Repo
+
+Whdigo is an offline password manager. It does not interact with a server and should be run offline (no internet connection).
+Apps and Sign On credentials are mapped together. 
+An offline file should be saved (either 1 or more secure are 2 files which are bother needed to rebuild the repo).
+
+One file
+All javascript and css is embedded in one file - index.html.
+If you need to run as a PWA you will need to download the manifest.json and the service worker sw.js along with the icons.
+
+MIT
---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'encrypted-mapper-cache-v1-5';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png',
+    '/icons/whdigo-transparent-square-logo.png',
+    '/icons/whdigo-words-transparent.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.mode === 'navigate') {
+        // This handles navigation requests (index.html)
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const copy = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put('/', copy));
+                    return response;
+                })
+                .catch(() => caches.match('/index.html'))
+        );
+    } else {
+        // Other files: cache-first
+        event.respondWith(
+            caches.match(event.request)
+                .then(response => response || fetch(event.request))
+        );
+    }
+});
